7198ba511114bbb34de05b368c00b0fa62e6e078
Sankapl Mishra <62585077+sankalpmishra2795@users.noreply.github.com>
2021-10-07 13:03:25 +0530

    Update NewsApi.js

--- src/Api/NewsApi.js
+++ src/Api/NewsApi.js
@@ -2,7 +2,7 @@ import axios  from "axios"
 
 
 export const getNewsList = () => {
-    const url = "https://newsapi.org/v2/everything?q=tesla&from=2021-09-06&sortBy=publishedAt&apiKey=7c20644e49d5418c88e417a34f55636a"
+    const url = "https://newsapi.org/v2/everything?q=tesla&from=2021-09-07&sortBy=publishedAt&apiKey=7c20644e49d5418c88e417a34f55636a"
     const response = axios({
       url: url,
       method: "get",
